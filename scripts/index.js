38b701cda23094e4e571f594fb632acc1d39ee10
Chayana Norbu <norchayan@yandex.ru>
2024-11-07 02:45:15 +0300

    5 sprint_1

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,44 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
+
+// Функция создания карточки
+function createCard(cardData, handleDeleteClick) {
+  // Клонируем шаблон карточки
+  const cardElement = cardTemplate.cloneNode(true);
+
+  // Настраиваем значения карточки
+  const cardImage = cardElement.querySelector('.card__image');
+  const cardTitle = cardElement.querySelector('.card__title');
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+  cardTitle.textContent = cardData.name;
+
+  // Добавляем обработчик клика на кнопку удаления
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', handleDeleteClick);
+
+  // Возвращаем готовый элемент карточки
+  return cardElement;
+}
+
+// Функция удаления карточки
+function deleteCard(cardElement) {
+  // Удаляем карточку из DOM
+  placesList.removeChild(cardElement);
+}
+
+// Выводим карточки на страницу
+initialCards.forEach(card => {
+  // Создаем карточку
+  const cardElement = createCard(card, () => {
+    // Удаляем карточку при клике на кнопку "Удалить"
+    deleteCard(cardElement);
+  });
+
+  // Добавляем карточку в список
+  placesList.append(cardElement);
+});
